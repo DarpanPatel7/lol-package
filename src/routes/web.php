a6106ab851eaba79d4528267dfdcc1ccc1c6508b
Darpan Patel <darpan.semicolon@gmail.com>
2022-01-20 17:12:16 +0530

    web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '<div style="display: flex; justify-content: center; height: 100vh; width:100%; min-height: 100%; min-width: 100%;"><img src="https://c.tenor.com/hHnl09qhfk0AAAAC/lick-birdy-flirty.gif" alt="this slowpoke moves" /></div>';
 });
+
+Route::get('lol', function () {
+    return '<div style="display: flex; justify-content: center; height: 100vh; width:100%; min-height: 100%; min-width: 100%;"><img src="https://c.tenor.com/hHnl09qhfk0AAAAC/lick-birdy-flirty.gif" alt="this slowpoke moves" /></div>';
+});
